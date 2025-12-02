023f4e96d52079dd82a42d2c5014e66a42b1a385
Mohammedcode007 <code.hassan.1992@gmail.com>
2025-12-02 17:42:05 +0200

    78

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,27 @@ const generateToken = require('../utils/generateToken');
 // Register
 const registerUser = async (req, res, next) => {
   try {
-    const { name, email, password } = req.body;
-    const userExists = await User.findOne({ email });
-    if (userExists) return res.status(400).json({ message: 'User already exists' });
+    const { name, email, phone, password } = req.body;
 
-    const user = await User.create({ name, email, password });
+    // التحقق من وجود الإيميل
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // التحقق من وجود رقم الهاتف
+    const phoneExists = await User.findOne({ phone });
+    if (phoneExists) {
+      return res.status(400).json({ message: 'Phone number already used' });
+    }
+
+    const user = await User.create({ name, email, phone, password });
+
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      phone: user.phone,
       token: generateToken(user._id)
     });
   } catch (err) {
@@ -24,17 +36,21 @@ const registerUser = async (req, res, next) => {
 const authUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email });
+
     if (user && await user.matchPassword(password)) {
-      res.json({
+      return res.json({
         _id: user._id,
         name: user.name,
         email: user.email,
+        phone: user.phone,
         token: generateToken(user._id)
       });
-    } else {
-      res.status(401).json({ message: 'Invalid email or password' });
     }
+
+    res.status(401).json({ message: 'Invalid email or password' });
+
   } catch (err) {
     next(err);
   }
--- models/User.js
+++ models/User.js
@@ -5,20 +5,21 @@ const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true }, // رقم الهاتف
     password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-// pre-save hook: async بدون next
-userSchema.pre('save', async function() {
+// pre-save hook
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
 // method للتحقق من الباسورد
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
